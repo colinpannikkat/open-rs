4230487b2332902d1e00aecc9e781e62dfd651f9
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-05-19 16:05:19 -0700

    Add eval scripts for lora models

--- eval_bench_lora.sh
+++ eval_bench_lora.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+
+export ACCELERATE_LOG_LEVEL=info
+export TRITON_CACHE_DIR=".triton"
+
 # Base model configuration
 # MODEL="./data/OpenRS-GRPO/checkpoint-300"
 # MODEL="knoveleng/Open-RS3"
-BASE_MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
+MODEL="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B" #"./data/OpenRS-RLoRA-LoftQ"
+LORA="./data/OpenRS-RLoRA-LoftQ"
+BASE_MODEL_ARGS="pretrained=$MODEL,lora_path=$LORA,dtype=bfloat16,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
 
-OUTPUT_DIR="logs/evals/full-math-bench-tasks/$MODEL"
+OUTPUT_DIR="logs/evals/full-math-bench-tasks/$LORA"
 
 mkdir -p "$OUTPUT_DIR"
 
---
+++ eval_mmlu_lora.sh
@@ -0,0 +1,13 @@
+export ACCELERATE_LOG_LEVEL=info
+export TRITON_CACHE_DIR=".triton"
+
+TASK="helm|mmlu|0|0"
+MODEL="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B" #"./data/OpenRS-RLoRA-LoftQ"
+LORA="./data/OpenRS-RLoRA-LoftQ"
+BASE_MODEL_ARGS="pretrained=$MODEL,lora_path=$LORA,dtype=bfloat16,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
+
+OUTPUT_DIR="logs/evals/full-math-bench-tasks/$LORA"
+
+lighteval vllm $MODEL_ARGS "$TASK" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
